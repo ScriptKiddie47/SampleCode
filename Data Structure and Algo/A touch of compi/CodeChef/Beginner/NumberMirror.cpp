0c2221fc9b55d0992d466d040af2071e10a38b35
ScriptKiddie47 <ritambala@live.com>
2021-02-20 03:43:02 +0530

    Create NumberMirror.cpp

---
+++ Data Structure and Algo/A touch of compi/CodeChef/Beginner/NumberMirror.cpp
@@ -0,0 +1,26 @@
+/*Problem Statement
+Write a program that accepts a number, n, and outputs the same.
+
+Input
+The only line contains a single integer.
+
+Output
+Output the answer in a single line.
+
+Constraints
+0 = n = 105
+Sample Input
+123
+Sample Output
+123*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n;
+	cin >> n;
+	cout << n;
+	return 0;
+}
