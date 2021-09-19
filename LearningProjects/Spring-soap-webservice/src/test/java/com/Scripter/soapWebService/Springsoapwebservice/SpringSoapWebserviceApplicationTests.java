c7d202de03174da49af0d9335efa3f9315fbfe29
ScriptKiddie47 <ritambala@live.com>
2021-09-19 16:36:40 +0530

    Soap-web-service

---
+++ LearningProjects/Spring-soap-webservice/src/test/java/com/Scripter/soapWebService/Springsoapwebservice/SpringSoapWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Scripter.soapWebService.Springsoapwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoapWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
