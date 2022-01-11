9c7ec64ba5806b33123d6f2900f9ba3e04d1196c
ScriptKiddie47 <ritambala@live.com>
2022-01-11 12:04:05 +0530

    Update CommonConcepts.js

--- JS/CommonConcepts.js
+++ JS/CommonConcepts.js
@@ -3,3 +3,11 @@
 // Destructuring Objects
 // Spread Syntax
 
+console.log(typeof(1)); // number
+console.log(typeof(true)); // boolean
+console.log(typeof('Hello')); // string
+console.log(typeof(function(){})); // function
+console.log(typeof(null)); // object [ Controversy ]
+console.log(typeof({})); // object
+console.log(typeof(undefined)); //undefined
+console.log(NaN); // number [ Controversy ]
