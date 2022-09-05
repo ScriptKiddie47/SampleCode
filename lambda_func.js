284e8de429b8b786fab2bbfe89a63e09b1f132da
S Bala <59485946+ScriptKiddie47@users.noreply.github.com>
2022-09-05 20:03:09 +0530

    Create lambda_func.js

---
+++ lambda_func.js
@@ -0,0 +1,28 @@
+{ "principalId": "secretToken", "policyDocument": { "Version": "2012-10-17", "Statement": [{"Action": "execute-api:Invoke", "Resource": ["arn:aws:execute-api:ap-south-1:378475259575:qo7wb61rib/dev/GET/get-company-files"], "Effect": auth}] }}
+
+exports.handler = async (event) => {
+  console.log(event);
+  let response = {};
+  let auth = "deny";
+  if (event.authorizationToken === "secretToken") {
+    auth = "allow";
+  }
+
+  response = {
+    principalId: "secretToken",
+    policyDocument: {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Action: "execute-api:Invoke",
+          Resource: [
+            "arn:aws:execute-api:ap-south-1:378475259575:qo7wb61rib/*/*",
+          ],
+          Effect: auth,
+        },
+      ],
+    },
+  };
+
+  return response;
+};
