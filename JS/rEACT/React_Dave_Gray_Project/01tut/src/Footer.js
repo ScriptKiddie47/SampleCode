14a78b865a2d211f18fe981d7b667b97fd6c23cc
syndicate <ritambala@live.com>
2024-01-03 14:09:06 +0530

    js-promise

--- JS/rEACT/React_Dave_Gray_Project/01tut/src/Footer.js
+++
@@ -1,8 +0,0 @@
-const Footer = ({ length }) => {
-    return (
-        <footer>
-            <p>List Item{length > 0 ? "s = " + length : null}</p>
-        </footer>
-    )
-}
-export default Footer;
--- JS/rEACT/React_Dave_Gray_Project/second-react-challenge-app/src/Cell.js
+++
@@ -1,8 +0,0 @@
-const Cell = ({ cellData }) => {
-    return ( 
-        <td>
-            {cellData}
-        </td>
-    );
-}
-export default Cell;
